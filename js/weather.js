7fc354fa233028e22aefcdeaf58524866c6e0a4d
huxiangze <15762065812@163.com>
2023-02-14 15:18:27 +0800

    Site updated: 2023-02-14 15:18:20

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "ed95105bcd7e4f19a380e089a0c044ce"
+  }
+}
